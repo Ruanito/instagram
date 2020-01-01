4c2362004525037547040f8511f39e3e481c44f4
Ruanito Santos <ruanito.santos@ebanx.com>
2020-01-01 13:53:55 -0300

    Upgrade user index layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :authenticate, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_user, only: [:index, :show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
